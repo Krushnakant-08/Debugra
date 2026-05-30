65f464196115c3ac8061cbd1a02a6b3bcc288756
Diya-od <diyaodedara245@gmail.com>
2026-05-30 09:57:46 +0530

    Added Eye toggle button in both sign in/ login page

--- src/components/Landing/LandingPage.jsx
+++ src/components/Landing/LandingPage.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
 import {
   signInWithPopup,
   signInWithEmailAndPassword,
@@ -269,6 +270,7 @@ export default function LandingPage() {
   const [name, setName] = useState('');
   const [loading, setLoading] = useState(false);
   const [mobileMenu, setMobileMenu] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleGoogle = async () => {
     try {
@@ -752,24 +754,29 @@ export default function LandingPage() {
                 className="modal-input"
                 required
               />
+              <div className='password-wrapper'>
               <input
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 aria-label="Password"
                 placeholder="Password"
-                type="password"
+                type={showPassword ? 'text' : 'password'}
                 className="modal-input"
                 required
                 minLength={6}
               />
               <button
-                type="submit"
-                disabled={loading}
-                className="landing-btn-primary"
-                style={{ width: '100%', padding: '10px', marginTop: '4px' }}
+                type="button"
+                className="password-toggle"
+                onClick={() => setShowPassword(!showPassword)}
               >
-                {loading ? 'Please wait...' : isSignUp ? 'Create Account' : 'Sign In'}
-              </button>
+               {showPassword ? (
+      <EyeOff size={18} strokeWidth={2} />
+    ) : (
+      <Eye size={18} strokeWidth={2} />
+    )}
+  </button>
+</div>
             </form>
 
             <p className="modal-toggle">
